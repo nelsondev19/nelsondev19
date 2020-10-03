b1c170ee4c1547433318fcfa2b17922e82753d3b
Nelson-developer <nelsonher019@gmail.com>
2020-10-03 17:42:32 -0600

    Mostrando las publicacciones de dev.to

--- README.md.tpl
+++ README.md.tpl
@@ -7,20 +7,29 @@
 Hola, Soy Nelson Hernandez 😀
 </p>
 <p align="center">
+<a href="https://dev.to/nelsonher019" target="blank">
+    <img align="center" src="./dev-brands.svg" alt="nelsonher019" height="28px" width="28px" />
+  </a>
   <a href="https://www.instagram.com/nelsonhermendz/" target="blank">
-    <img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/instagram.svg" alt="Nelson-developer" height="28px" width="28px" />
+    <img align="center" src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/instagram.svg" alt="nelsonher019" height="28px" width="28px" />
   </a>
    <a href="https://medium.com/@nelsonher019" target="blank">
-    <img align="center" src="https://cdn-images-1.medium.com/max/1200/1*6_fgYnisCa9V21mymySIvA.png" alt="Nelson-developer" height="28px" width="28px" />
+    <img align="center" src="https://cdn-images-1.medium.com/max/1200/1*6_fgYnisCa9V21mymySIvA.png" alt="nelsonher019" height="28px" width="28px" />
   </a>
 </p>
 
 
-Soy **FullStack Developer**, me apasiona el desarrollo web desde los 15 años, me gusta trabajar con herramientas como **Vue JS**, **React JS**, **Express JS**, **Python (Flask)**, **MongoDB**, etc.
+Soy **Frontend Developer**, me apasiona el desarrollo web y enseñar a las personas a que conozcan lo nuevo de la tecnologia 
 
-#### 📹 Publicaciones actuales en [Medium](https://medium.com/@nelsonher019):
+####  Publicaciones actuales en [Medium](https://medium.com/@nelsonher019):
 
 
-%{{latest_images_articles1}}%
-%{{latest_images_articles2}}%
-%{{latest_images_articles3}}%
+%{{latest_images_medium1}}%
+%{{latest_images_medium2}}%
+%{{latest_images_medium3}}%
+
+
+####  Publicaciones actuales en [Dev.to](https://dev.to/nelsonher019):
+
+%{{latest_images_devto1}}%
+%{{latest_images_devto2}}%
--- index.js
+++ index.js
@@ -1,34 +1,56 @@
 const fs = require("fs").promises;
 const axios = require("axios").default;
 
-const LATEST_IMAGES_ARTICLES1 = "%{{latest_images_articles1}}%";
-const LATEST_IMAGES_ARTICLES2 = "%{{latest_images_articles2}}%";
-const LATEST_IMAGES_ARTICLES3 = "%{{latest_images_articles3}}%";
+const LATEST_IMAGES_MEDIUM1 = "%{{latest_images_medium1}}%";
+const LATEST_IMAGES_MEDIUM2 = "%{{latest_images_medium2}}%";
+const LATEST_IMAGES_MEDIUM3 = "%{{latest_images_medium3}}%";
+
+const LATEST_IMAGES_DEVTO1 = "%{{latest_images_devto1}}%";
+const LATEST_IMAGES_DEVTO2 = "%{{latest_images_devto2}}%";
 
 (async () => {
-  const markdownTemplate = await fs.readFile("README.md.tpl", {
+  const markdownTemplate = await fs.readFile("./README.md.tpl", {
     encoding: "utf-8",
   });
 
-  const response = await axios.get(
+  const responseMedium = await axios.get(
     "https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fmedium.com%2Ffeed%2F%40nelsonher019"
   );
-  const posts = response.data.items;
+  const posts = responseMedium.data.items;
 
-  const imagesPost1 = `<a href="${posts[0].link}" target='_blank'>
+  const responseDevTo = await axios.get(
+    "https://dev.to/api/articles?username=nelsonher019"
+  );
+  postDevTo = responseDevTo.data;
+
+  //MEDIUM
+  const mediumPost1 = `<a href="${posts[0].link}" target='_blank'>
     <img width='30%' src=${posts[0].thumbnail} alt="${posts[0].title}" />
   </a>`;
-  const imagesPost2 = `<a href="${posts[1].link}" target='_blank'>
+  const mediumPost2 = `<a href="${posts[1].link}" target='_blank'>
     <img width='30%' src="${posts[1].thumbnail}" alt="${posts[1].title}" />
   </a>`;
-  const imagesPost3 = `<a href="${posts[2].link}" target='_blank'>
+  const mediumPost3 = `<a href="${posts[2].link}" target='_blank'>
     <img width='30%' src="${posts[2].thumbnail}" alt="${posts[2].title}" />
   </a>`;
 
+  //DEV.TO
+
+  const devtoPost1 = `<a href="${postDevTo[0].url}" target='_blank'>
+    <img width='30%' src=${postDevTo[0].cover_image} alt="${postDevTo[0].title}" />
+  </a>`;
+  const devtoPost2 = `<a href="${postDevTo[1].url}k}" target='_blank'>
+    <img width='30%' src="${postDevTo[1].cover_image}" alt="${postDevTo[1].title}" />
+  </a>`;
+
   const newMarkdown = markdownTemplate
-    .replace(LATEST_IMAGES_ARTICLES1, imagesPost1)
-    .replace(LATEST_IMAGES_ARTICLES2, imagesPost2)
-    .replace(LATEST_IMAGES_ARTICLES3, imagesPost3);
+    //medium
+    .replace(LATEST_IMAGES_MEDIUM1, mediumPost1)
+    .replace(LATEST_IMAGES_MEDIUM2, mediumPost2)
+    .replace(LATEST_IMAGES_MEDIUM3, mediumPost3)
+    //devto
+    .replace(LATEST_IMAGES_DEVTO1, devtoPost1)
+    .replace(LATEST_IMAGES_DEVTO2, devtoPost2);
 
   await fs.writeFile("./README.md", newMarkdown);
 })();
