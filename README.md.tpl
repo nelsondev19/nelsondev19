9b870b4259c1fed46bd2ab7c6950b4a31dbacec4
Nelson-developer <nelsonher019@gmail.com>
2021-08-14 22:06:38 -0600

    code of lambda function

--- README.md.tpl
+++ README.md.tpl
@@ -1,8 +1,3 @@
-
-- 📙 [Blogger in Medium](https://nelsoncode019.medium.com/)
-- <img src="https://cdn.jsdelivr.net/npm/simple-icons@3.0.1/icons/instagram.svg" alt="Perfil de Instagram de NelsonCode" height="15px" width="15px" /> [Instagram de desarrollo web](https://www.instagram.com/nelsoncode)
-- 💻 [Diseños con HTML Y CSS](https://codepen.io/nelsoncode019)
-
 ###  Publicaciones actuales en [Youtube](https://www.youtube.com/channel/UCNtGnenu3-E363hcijzVt0w/featured)
 
 %{{latest_images_youtube1}}%
--- index.js
+++ index.js
@@ -10,8 +10,7 @@ const LATEST_IMAGES_YOUTUBE1 = "%{{latest_images_youtube1}}%";
 const LATEST_IMAGES_YOUTUBE2 = "%{{latest_images_youtube2}}%";
 const LATEST_IMAGES_YOUTUBE3 = "%{{latest_images_youtube3}}%";
 
-console.log("el valor obtenido es el siguiente " + process.env.CHANNELID);
-(async () => {
+exports.handler = async () => {
   try {
     const markdownTemplate = await fs.readFile("./README.md.tpl", {
       encoding: "utf-8",
@@ -26,29 +25,31 @@ console.log("el valor obtenido es el siguiente " + process.env.CHANNELID);
       `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${process.env.CHANNELID}&maxResults=10&order=date&type=video&key=${process.env.API_KEY}`
     );
 
-    youtubeItems = responseYoutube.data.items;
+    const youtubeItems = responseYoutube.data.items;
+
+    console.log(youtubeItems);
 
     //MEDIUM
     const mediumPost1 = `<a href="${posts[0].link}" target='_blank'>
-      <img width='30%' src=${posts[0].thumbnail} alt="${posts[0].title}" />
-    </a>`;
+  <img width='30%' src=${posts[0].thumbnail} alt="${posts[0].title}" />
+</a>`;
     const mediumPost2 = `<a href="${posts[1].link}" target='_blank'>
-      <img width='30%' src="${posts[1].thumbnail}" alt="${posts[1].title}" />
-    </a>`;
+  <img width='30%' src="${posts[1].thumbnail}" alt="${posts[1].title}" />
+</a>`;
     const mediumPost3 = `<a href="${posts[2].link}" target='_blank'>
-      <img width='30%' src="${posts[2].thumbnail}" alt="${posts[2].title}" />
-    </a>`;
+  <img width='30%' src="${posts[2].thumbnail}" alt="${posts[2].title}" />
+</a>`;
 
     // YOUTUBE
     const youtubePost1 = `<a href="https://www.youtube.com/watch?v=${youtubeItems[0].id.videoId}" target='_blank'>
-     <img width='30%' src="${youtubeItems[0].snippet.thumbnails.high.url}" alt="${youtubeItems[0].title}" />
-   </a>`;
+ <img width='30%' src="${youtubeItems[0].snippet.thumbnails.high.url}" alt="${youtubeItems[0].snippet.title}" />
+</a>`;
     const youtubePost2 = `<a href="https://www.youtube.com/watch?v=${youtubeItems[1].id.videoId}" target='_blank'>
-     <img width='30%' src="${youtubeItems[1].snippet.thumbnails.high.url}" alt="${youtubeItems[1].title}" />
-   </a>`;
+ <img width='30%' src="${youtubeItems[1].snippet.thumbnails.high.url}" alt="${youtubeItems[1].snippet.title}" />
+</a>`;
     const youtubePost3 = `<a href="https://www.youtube.com/watch?v=${youtubeItems[2].id.videoId}" target='_blank'>
-     <img width='30%' src="${youtubeItems[2].snippet.thumbnails.high.url}" alt="${youtubeItems[2].title}" />
-   </a>`;
+ <img width='30%' src="${youtubeItems[2].snippet.thumbnails.high.url}" alt="${youtubeItems[2].snippet.title}" />
+</a>`;
 
     const newMarkdown = markdownTemplate
       // MEDIUM
@@ -59,8 +60,8 @@ console.log("el valor obtenido es el siguiente " + process.env.CHANNELID);
       .replace(LATEST_IMAGES_YOUTUBE1, youtubePost1)
       .replace(LATEST_IMAGES_YOUTUBE2, youtubePost2)
       .replace(LATEST_IMAGES_YOUTUBE3, youtubePost3);
-    await fs.writeFile("./README.md", newMarkdown);
+    return newMarkdown;
   } catch (error) {
     console.log("Ocurrió un error " + error);
   }
-})();
+};
